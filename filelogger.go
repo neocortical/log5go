a8b815c04be6c92f92596af1b3dd4ba6fa104ed8
Nathan Smith <nate@branchout.com>
2014-08-10 22:28:14 -0700

    file logger works

--- filelogger.go
+++ filelogger.go
@@ -1,6 +1,57 @@
 package log4go
 
+import (
+  "os"
+  "path/filepath"
+  "sync"
+  "time"
+)
+
 const (
   defaultDirectoryPerms = 0755
   defaultFilePerms      = 0644
 )
+
+type fileAppender struct {
+  lock sync.Mutex
+  f *os.File
+}
+
+var fileAppenderMap = make(map[string]*fileAppender)
+var fileAppenderMapLock = sync.Mutex{}
+
+func (a *fileAppender) Append(msg string, level LogLevel, tstamp time.Time) {
+  a.lock.Lock()
+  defer a.lock.Unlock()
+
+  a.f.Write([]byte(msg))
+}
+
+func NewFileLogger(dir string, filename string, level LogLevel, timePrefix string) (_ Log4Go, err error) {
+  expandedDir, err := filepath.Abs(filepath.Dir(dir))
+  if err != nil {
+    return nil, err
+  }
+
+  fullFilename := filepath.Join(expandedDir, filename)
+
+  fileAppenderMapLock.Lock()
+  defer fileAppenderMapLock.Unlock()
+
+  var appender *fileAppender = fileAppenderMap[fullFilename]
+  if appender == nil {
+    logfile, err := os.Create(fullFilename)
+    if err != nil {
+      return nil, err
+    }
+    appender = &fileAppender{sync.Mutex{}, logfile}
+    fileAppenderMap[fullFilename] = appender
+  }
+
+  result := stdLogger{
+    appender,
+    level,
+    timePrefix,
+  }
+  return Log4Go(&result), nil
+}
