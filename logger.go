e20f6bb048fd72499f883ee778532cdfa3f57ea7
Nathan Smith <nate@branchout.com>
2014-08-11 17:34:37 -0700

    moved log levels in own go file

--- logger.go
+++ logger.go
@@ -5,18 +5,6 @@ import (
 	"time"
 )
 
-type LogLevel uint16
-
-const (
-	LogAll   LogLevel = 0
-	LogTrace LogLevel = 100
-	LogDebug LogLevel = 200
-	LogInfo  LogLevel = 300
-	LogWarn  LogLevel = 400
-	LogError LogLevel = 500
-	LogFatal LogLevel = 600
-)
-
 const (
 	TF_NCSA  = "02/Jan/2006:15:04:05 -0700"
 	TF_GoStd = "2006/01/02 15:04:05"
@@ -38,16 +26,6 @@ type stdLogger struct {
 	appender   appender
 	level      LogLevel
 	timePrefix string
-}
-
-var levelMap = map[LogLevel]string{
-	LogAll:   "LOG",
-	LogTrace: "TRACE",
-	LogDebug: "DEBUG",
-	LogInfo:  "INFO",
-	LogWarn:  "WARN",
-	LogError: "ERROR",
-	LogFatal: "FATAL",
 }
 
 func (l *stdLogger) Log(level LogLevel, format string, a ...interface{}) {
@@ -95,8 +73,4 @@ func (l *stdLogger) GetLogLevel() LogLevel {
 
 func (l *stdLogger) SetLogLevel(level LogLevel) {
 	l.level = level
-}
-
-func RegisterLogLevel(level LogLevel, prefix string) {
-  levelMap[level] = prefix
 }
---
+++ loglevel.go
@@ -0,0 +1,30 @@
+package log4go
+
+type LogLevel uint16
+
+// Standard log levels (lifted directly from log4j)
+const (
+  LogAll   LogLevel = 0
+  LogTrace LogLevel = 100
+  LogDebug LogLevel = 200
+  LogInfo  LogLevel = 300
+  LogWarn  LogLevel = 400
+  LogError LogLevel = 500
+  LogFatal LogLevel = 600
+)
+
+// maps log levels to prefix strings describing each. extensible
+var levelMap = map[LogLevel]string{
+  LogAll:   "LOG",
+  LogTrace: "TRACE",
+  LogDebug: "DEBUG",
+  LogInfo:  "INFO",
+  LogWarn:  "WARN",
+  LogError: "ERROR",
+  LogFatal: "FATAL",
+}
+
+// replace a log level prefix string, or add one for a custom log level
+func RegisterLogLevel(level LogLevel, prefix string) {
+  levelMap[level] = prefix
+}
