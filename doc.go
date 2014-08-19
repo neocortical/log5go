f19e647b6152738cc24aa52e15511a3440f060bc
Nathan Smith <nate@branchout.com>
2014-08-19 14:25:22 -0700

    release 0.13.4: performance improvements

--- doc.go
+++ doc.go
@@ -1,42 +1,46 @@
 /*
-Package log5go is a simple, powerful logging framework for Go, loosely based on Java's log4j.
-Loggers support log4j's basic log levels out of the box: TRACE, DEBUG, INFO, WARN, ERROR,
-and FATAL, but additional levels can be integrated into the framework while still respecting
-the level hierarchy. log5go can log to the console or to one or more files and supports
-log file archiving and rotation.
+Package log5go is a powerful, configurable logging framework for Go.
+
+log5go features clear, declarative configuration, 6 standard log levels (based on Java's Log4j),
+logfile rotation, and highly configurable formats and appenders (file, console, Writer, etc.).
+
+log5go loggers can be added to and retrieved from a registry, removing the need to
+pass loggers around your code or rely on global logger variables.
+
 
 Basics
 
-Loggers are configured using a builder pattern starting with Logger(LogLevel), optionally
-terminating with Register(key string). Register() registers the logger internally
-using the supplied key. This results in loggers
-that can be statically retrieved in other parts of the package using GetLog(string).
+Loggers are configured using a builder pattern starting with Logger(LogLevel) followed by
+any of a rich set of configuration methods. Want JSON? Use .Json(). Want logging to a file
+with daily rotation? Use .ToFile(dir, fname).WithRotation(frequency, numSaved)
+
+You can configure a number of different format and destination options with a single,
+easy-to-read line of code.
+
 
 Examples
 
-The following example creates a file logger and registers it with the name "db":
+Create a logger that logs JSON to stderr:
 
-  log, err := log5go.Logger(log5go.LogDebug).ToFile("/var/log", "myprog_db.log").Register("db")
+  log, err := log5go.Logger(log5go.LogInfo).Json()
 
-All package local code will be able to retrieve the same logger by calling:
+Create a file logger with an hourly log rotation and 10 saved archive files:
 
-  log, err := log5go.GetLog("db")
+  log, err := log5go.Logger(log5go.LogDebug).ToFile("/var/log", "db.log").WithRotation(log5go.RollHourly, 10)
 
-This allows logging to be unobtrusive in code, since any package-local code can easily
-obtain the desired logger without the need to create a global variable.
+Register a stdout logger and retrieve it in another part of your code:
 
-The following example creates a file logger with a log rotation scheme:
+  // create and register
+  log, err := log5go.Logger(log5go.LogAll).ToStdout().Register("mylog")
 
-  log := log5go.Logger(log5go.LogAll).ToFile("/var/log", "myprog.log").WithRotation(log5go.RollDaily, 7)
+  // get it from the registry
+  log, err := log5go.GetLog("mylog")
 
-In this example, the logger will archive the log file daily at midnight, maintaining a maximum
-of 7 archived log files. (A timestamp is appended to the name of each log file and an attempt is
-made to delete the file that was created 8 days ago.)
 */
 package log5go
 
 // Package version info
-const VERSION = "0.13.3"
+const VERSION = "0.13.4"
 const MAJOR_VERSION = 0
 const MINOR_VERSION = 13
-const PATCH_VERSION = 3
+const PATCH_VERSION = 4
