457c51d4a88eddc9f16e7a30b0c274b22f2876f6
Karl Kastan <karlkastan@gmail.com>
2015-12-16 15:02:58 -0800

    load conf from env once and log to console what the conf is

--- default.go
+++ default.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 const (
@@ -14,6 +15,25 @@ const (
 	L5G_LOG_FILE_ROTATION_KEEP_N_FILES = "L5G_LOG_FILE_ROTATION_KEEP_N_FILES"
 )
 
+type logconf struct {
+	logLevel             LogLevel
+	logFilePath          string
+	logFileName          string
+	logFileRollFrequency rollFrequency
+	keepNFiles           int
+	logLineLength        string
+}
+
+var lock = sync.RWMutex{}
+var conf *logconf
+
+func GetConsoleLogger() (l Log5Go) {
+	l = GetOrCreate("console", func() (_ Log5Go) {
+		return Logger(LogAll).ToStdout()
+	})
+	return
+}
+
 func GetLogger(key string) (l Log5Go) {
 	l = GetOrCreate(key, func() (_ Log5Go) {
 		return createLogFromEnvVars()
@@ -21,54 +41,85 @@ func GetLogger(key string) (l Log5Go) {
 	return
 }
 
-func createLogFromEnvVars() (l Log5Go) {
-	l = getBaseLogWithLevel(os.Getenv(L5G_LOG_LEVEL))
+func loadEnv() {
+	lock.Lock()
+	defer lock.Unlock()
 
-	path, file := parseFilenameAndPath(os.Getenv(L5G_LOG_FILE_NAME))
-	if path != "" && file != "" {
-		l.ToFile(path, file)
+	if conf == nil {
+		console := GetConsoleLogger()
+		console.Info("Initializing Log5Go ... ")
 
-		n := parseKeepNFilesInt(os.Getenv(L5G_LOG_FILE_ROTATION_KEEP_N_FILES))
-		// file rotation
-		switch os.Getenv(L5G_LOG_FILE_ROTATION_FREQUENCY) {
-		case "MINUTE":
-			l.WithRotation(RollMinutely, n)
-		case "HOUR":
-			l.WithRotation(RollHourly, n)
-		case "DAY":
-			l.WithRotation(RollDaily, n)
-		case "WEEK":
-			l.WithRotation(RollWeekly, n)
+		conf = &logconf{
+			logLevel: parseLogLevel(os.Getenv(L5G_LOG_LEVEL)),
 		}
 
+		console.Info("[Log Level: %v]", levelMap[conf.logLevel])
+
+		conf.logFilePath, conf.logFileName = parseFilenameAndPath(os.Getenv(L5G_LOG_FILE_NAME))
+		if conf.logFilePath != "" && conf.logFileName != "" {
+			console.Info("[Logging to file: %s/%s]", conf.logFilePath, conf.logFileName)
+			conf.keepNFiles = parseKeepNFilesInt(os.Getenv(L5G_LOG_FILE_ROTATION_KEEP_N_FILES))
+			logFileRollFrequency, rollLabel := parseFileRotationFrequency(os.Getenv(L5G_LOG_FILE_ROTATION_FREQUENCY))
+			conf.logFileRollFrequency = logFileRollFrequency
+
+			console.Info("[Logfile Roll Frequency: %s]", rollLabel)
+			console.Info("[Logfiles to keep: %d]", conf.keepNFiles)
+
+		} else {
+			console.Info("[Logging to Stdout]")
+		}
+
+		conf.logLineLength = parseLogLineLength(os.Getenv(L5G_LOG_LINE_LENGTH))
+		console.Info("[Log Line Length: %s]", conf.logLineLength)
+	}
+}
+
+func createLogFromEnvVars() (l Log5Go) {
+	loadEnv()
+
+	// create a base Log5Go with the appropriate log level
+	l = Logger(conf.logLevel)
+
+	if conf.logFilePath != "" && conf.logFileName != "" {
+		l.ToFile(conf.logFilePath, conf.logFileName)
+
+		// file rotation
+		if conf.logFileRollFrequency != RollNone {
+			l.WithRotation(conf.logFileRollFrequency, conf.keepNFiles)
+		}
 	} else {
 		// default to Stdout
 		l.ToStdout().WithStderr()
 	}
 
-	switch os.Getenv(L5G_LOG_LINE_LENGTH) {
-	case "NONE":
-		// no-op
+	switch conf.logLineLength {
 	case "LONG":
 		l.WithLongLines()
-	case "SHORT":
-		l.WithShortLines()
-	case "":
+	case "SHORT", "NONE":
 		l.WithShortLines()
 	}
 
 	return
 }
 
-func getBaseLogWithLevel(loglevel string) (l Log5Go) {
+func parseLogLineLength(token string) (str string) {
+	str = "NONE"
+	switch token {
+	case "LONG", "SHORT":
+		str = token
+	}
+	return str
+}
+
+func parseLogLevel(logLevelStr string) (level LogLevel) {
 	for k, v := range levelMap {
-		if v == loglevel {
-			l = Logger(k)
+		if v == logLevelStr {
+			level = k
 			return
 		}
 	}
 
-	l = Logger(LogAll)
+	level = LogAll
 	return
 }
 
@@ -86,6 +137,26 @@ func parseFilenameAndPath(fullname string) (path, filename string) {
 	path = fullname[:i]
 	filename = fullname[i+1:]
 	return
+}
+
+func parseFileRotationFrequency(str string) (freq rollFrequency, label string) {
+	label = str
+	switch str {
+	case "MINUTE":
+		freq = RollMinutely
+		return
+	case "HOUR":
+		freq = RollHourly
+		return
+	case "DAY":
+		freq = RollDaily
+		return
+	case "WEEK":
+		freq = RollWeekly
+		return
+	}
+
+	return RollNone, "NONE"
 }
 
 func parseKeepNFilesInt(str string) (i int) {
--- default_test.go
+++ default_test.go
@@ -6,18 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_getBaseLogWithLevel_Default(t *testing.T) {
-	l := getBaseLogWithLevel("")
-	if assert.NotNil(t, l) {
-		assert.Equal(t, LogAll, l.LogLevel())
-	}
+func Test_parseLogLevel_Default(t *testing.T) {
+	assert.Equal(t, LogAll, parseLogLevel(""))
 }
 
-func Test_getBaseLogWithLevel_LogNotice(t *testing.T) {
-	l := getBaseLogWithLevel("NOTICE")
-	if assert.NotNil(t, l) {
-		assert.Equal(t, LogNotice, l.LogLevel())
-	}
+func Test_parseLogLevel_LogNotice(t *testing.T) {
+	assert.Equal(t, LogNotice, parseLogLevel("NOTICE"))
 }
 
 func Test_parseFilenameAndPath_Empty(t *testing.T) {
@@ -52,4 +46,24 @@ func Test_parseKeepNFilesInt_1(t *testing.T) {
 
 func Test_parseKeepNFilesInt_NotNumeric(t *testing.T) {
 	assert.Equal(t, 1, parseKeepNFilesInt("foobar"))
+}
+
+func Test_parseLogLineLength_Default(t *testing.T) {
+	assert.Equal(t, "NONE", parseLogLineLength(""))
+}
+
+func Test_parseLogLineLength_Bad(t *testing.T) {
+	assert.Equal(t, "NONE", parseLogLineLength("FOO"))
+}
+
+func Test_parseLogLineLength_None(t *testing.T) {
+	assert.Equal(t, "NONE", parseLogLineLength("NONE"))
+}
+
+func Test_parseLogLineLength_Long(t *testing.T) {
+	assert.Equal(t, "LONG", parseLogLineLength("LONG"))
+}
+
+func Test_parseLogLineLength_Short(t *testing.T) {
+	assert.Equal(t, "SHORT", parseLogLineLength("SHORT"))
 }
